e10eba2d62804c76a804efa5b764878d3236a2c6
Fabricio Araujo Pereira <69161533+Fabricio-A-Pereira@users.noreply.github.com>
2021-02-09 11:19:12 -0300

    Create Conversão de Temperatura.py

---
+++ Entrada de Dados/Conversão de Temperatura.py
@@ -0,0 +1,6 @@
+tempFah = input("\nDigite uma temperatura em Fahrenheit: ")
+tempFah = float(tempFah)
+      # pode converter direto na linha do input
+tempCel = (tempFah - 32) * 5 / 9
+
+print("A temperatura em Celsius é:", tempCel)
